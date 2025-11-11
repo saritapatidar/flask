2ce73edffed92de0277863ea25df921c5eebb8b6
sarita <saritapatidar@thoughtwin.com>
2025-11-11 18:05:43 +0530

    remove commit

--- main.py
+++ main.py
@@ -5,9 +5,3 @@ app = create_app()
 if __name__ == '__main__':
     app.run(debug=True)
 
-<<<<<<< Updated upstream
-## The above code initializes and runs a Flask application by importing the create_app function from the app package.
-=======
-
-# sarita
->>>>>>> Stashed changes
