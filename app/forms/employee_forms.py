d74e4c2a2b6dbf52f1f56847a46ce38955fdc047
sarita <saritapatidar@thoughtwin.com>
2025-11-12 17:39:17 +0530

    registration/login

--- app/forms/employee_forms.py
+++ app/forms/employee_forms.py
@@ -5,3 +5,11 @@ class EmployeeForm(FlaskForm):
     name = StringField('Name')
     age = IntegerField('Age')
     department = StringField('Department')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username')
+    password = StringField('Password')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username')
+    password = StringField('Password')
