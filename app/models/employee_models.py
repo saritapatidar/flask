bb709ba4c929ebb0e889a54f7fd988252302e165
sarita <saritapatidar@thoughtwin.com>
2025-12-10 16:46:20 +0530

    second commit

--- app/models/employee_models.py
+++ app/models/employee_models.py
@@ -13,3 +13,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
