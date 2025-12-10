ec781996ab442358893b8276b0b60d8ba91d7194
sarita <saritapatidar@thoughtwin.com>
2025-12-10 18:11:18 +0530

    Revert "wright the code"
    This reverts commit c224257ea62e29ee71ffa82c9e7460ea60da3d26.

--- app/models/employee_models.py
+++ app/models/employee_models.py
@@ -7,21 +7,4 @@ class Employee(db.Model):
     age = db.Column(db.Integer, nullable=False)
     department = db.Column(db.String(50), nullable=False)
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), unique=True, nullable=False)
-
-
-class UserRole(db.Model):
-    __tablename__ = 'user_roles'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)   
+    
