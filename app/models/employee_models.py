f151378cbecd399de1928dd846d2a31fdd290d9a
sarita <saritapatidar@thoughtwin.com>
2025-12-10 17:21:11 +0530

    Add newline and format UserRole model in employee_models.py

--- app/models/employee_models.py
+++ app/models/employee_models.py
@@ -1,5 +1,6 @@
 from app.database import db 
 
+
 class Employee(db.Model):
     __tablename__ = 'employees'
     id = db.Column(db.Integer, primary_key=True)
@@ -19,3 +20,10 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
+
+
+class UserRole(db.Model):
+    __tablename__ = 'user_roles'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'), nullable=False)
